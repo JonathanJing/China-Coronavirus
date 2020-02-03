23ed2de375803e07082c82c13a1912c4072fd0d5
Jonathan Jing <JonathanJing@users.noreply.github.com>
2020-02-03 11:59:30 -0800

    2/4 update

--- fydata.R
+++ fydata.R
@@ -39,6 +39,7 @@ citydata2 <- citydata
 directcity <- which(provname %in% c('北京', '上海', '天津', '重庆'))
 citydata2 <- citydata2[-c(directcity),]
 
+#rbind not woking well with mapping function below, change to tibble add_row
 library(tibble)
 citydata2 <- add_row(citydata2, time = time, provname = '北京', city = '北京', citypop = provdata[1,3])
 citydata2 <- add_row(citydata2, time = time, provname = '上海', city = '上海', citypop = provdata[15,3])
@@ -46,6 +47,7 @@ citydata2 <- add_row(citydata2, time = time, provname = '天津', city = '天津
 citydata2 <- add_row(citydata2, time = time, provname = '重庆', city = '重庆', citypop = provdata[7,3])
 citydata2 <- add_row(citydata2, time = time, provname = '香港', city = '香港', citypop = provdata[29,3])
 citydata2 <- add_row(citydata2, time = time, provname = '澳门', city = '澳门', citypop = provdata[28,3])
+citydata2 <- add_row(citydata2, time = time, provname = '台湾', city = '台湾', citypop = provdata[27,3])
 
 #create new dataset for mapping
 #province level
@@ -56,17 +58,20 @@ citydatamap <- citydata2
 citydatamap[1,4] <- 1000 #Change confirmed population of Wuhan to 1000
 citydatamap[2,4] <- 1000 #Change confirmed population of Huanggang to 1000
 
+citydata3 <- citydata2
+citydata3[,4] <- log(citydata3[,4])
+
 library(leafletCN)
 geojsonMap(provdatamap, "china",
            namevar = ~provname, valuevar = ~provpop,
            popup =  paste0(provdata$provname, ":", provdata$provpop),
-           palette = "Reds", legendTitle = "省级确诊人口")
+           palette = "Reds", legendTitle = "省级确诊人数")
 
-geojsonMap(citydata2, "city",
+geojsonMap(citydata3, "city",
            namevar = ~city, valuevar = ~citypop,
            na.color = 'white', 
            popup =  paste0(citydata2$city, ":", citydata2$citypop),
-           palette = "Reds", legendTitle = "确诊人口")
+           palette = "Reds", legendTitle = "log(确诊人数)")
 
 ##daily data
 library(jsonlite)
